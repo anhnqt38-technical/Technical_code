802ec9a8417e63dd0b68e6b61d10f908fe749d27
Le Huu Tin <tin007253@gmail.com>
2026-08-18 10:30:59 +0700

    bai4

--- bai-4.java
+++ bai-4.java
@@ -3,7 +3,7 @@ public class Bai4 {
 }
 public static void main(String[] args) {
         
-        String chuoi_1 = "Ho Thi";
+        String chuoi_1 = "Ho Thi Thinh";
         String chuoi_2 = " Dim";
         String chuoi_3 = chuoi_1 + chuoi_2;
         System.out.printf("Ho: %s Ten: %s Ho va ten: %s", chuoi_1, chuoi_2, chuoi_3);
