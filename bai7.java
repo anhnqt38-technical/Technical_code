4328c61f82d06a24aebf9c6f6807b02b39d30d34
Zo Ram Nghiep <zoramnghiep2023@gmail.com>
2026-08-18 10:43:43 +0700

    feat: change

--- bai7.java
+++ bai7.java
@@ -4,5 +4,6 @@ import jdk.jshell.spi.ExecutionControl;
 public class Bai7 {
     public static void main(String[] args) {
         System.out.println("tin gay");
+        System.out.println("Phat deptrai");
     }
 }
