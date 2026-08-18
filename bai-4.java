e0d755b3b2608faca23e88b290fad8e51a944294
Ho Thi Dim <dimmiu51@gmail.com>
2026-08-18 10:06:01 +0700

    feature/bai4

---
+++ bai-4.java
@@ -0,0 +1,11 @@
+public class Bai4 {
+    
+}
+public static void main(String[] args) {
+        
+        String chuoi_1 = "Ho Thi";
+        String chuoi_2 = " Dim";
+        String chuoi_3 = chuoi_1 + chuoi_2;
+        System.out.printf("Ho: %s Ten: %s Ho va ten: %s", chuoi_1, chuoi_2, chuoi_3);
+    }
+
